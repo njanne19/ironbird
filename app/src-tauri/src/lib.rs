517aec1ace150ef8b4139679787f342e2dc2e3d1
Nick Janne <nickjanne7@gmail.com>
2026-04-25 22:57:54 -0700

    first commit. Libraries and basic crates setup

---
+++ app/src-tauri/src/lib.rs
@@ -0,0 +1,42 @@
+use ironbird_core::SimState;
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+#[specta::specta]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+#[specta::specta]
+fn get_state() -> SimState {
+    SimState {
+        position: [0.0, 0.0, 0.0],
+        attitude: [1.0, 0.0, 0.0, 0.0],
+        velocity: [0.0, 0.0, 0.0],
+    }
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+
+    let builder = tauri_specta::Builder::<tauri::Wry>::new()
+        .commands(tauri_specta::collect_commands![
+            greet,
+            get_state
+        ]);
+
+    #[cfg(debug_assertions)]
+    builder
+        .export(
+            specta_typescript::Typescript::default(),
+            "../src/bindings.ts"
+        )
+        .expect("Failed to export bindings");
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(builder.invoke_handler())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
